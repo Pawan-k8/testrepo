46b23c2827b5a2bc22aeb528a7f89b9317d7855c
Pawan-k8 <pawan.krishnamurthyy@gmail.com>
2026-06-12 07:56:29 +0530

    Update hello_world to return JSON format
    Refactor hello_world function to return JSON response.

--- flask.py
+++ flask.py
@@ -10,3 +10,34 @@ def hello_world():
     "function that handles request"
     return "hello world!"
 
+""" 
+"import Flask class"
+from flask import Flask, jsonify
+
+"create an instance of the Flask class"
+app = Flask(__name__)
+
+"define a route for the root URL"
+@app.route('/')
+def hello_world():
+    "function that handles request"
+    return jsonify (message="hello world!")
+
+"""
+
+or 
+
+""" 
+"import Flask class"
+from flask import Flask
+
+"create an instance of the Flask class"
+app = Flask(__name__)
+
+"define a route for the root URL"
+@app.route('/')
+def hello_world():
+    "function that handles request"
+    return {"message" : "hello world!"}
+
+"""
